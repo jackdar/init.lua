ab8e0f86d3930de32825f8ddb06bcaf90aa3c300
jackdar <jackdarlington2001@gmail.com>
2026-03-27 08:56:58 +1300

    fix: netrw hide to no longer hide all one and two character long files/dirs

--- after/ftplugin/netrw.lua
+++ after/ftplugin/netrw.lua
@@ -3,4 +3,4 @@ vim.opt_local.relativenumber = true
 vim.opt_local.signcolumn = "no"
 vim.g.netrw_localcopydircmd = "cp -r"
 vim.g.netrw_localmovedircmd = "cp -r"
-vim.g.netrw_list_hide = ".*.swp$,.DS_Store$,*/tmp/*,*.so,*.swp,*.zip,.git/,^..$,^...$"
+vim.g.netrw_list_hide = ".*\\.swp$,.DS_Store$,*/tmp/*,*.so,*.swp,*.zip,.git/,^\\./$,^\\.\\./$"
